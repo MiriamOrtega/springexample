3ac06be00b94e288f0d42de33896e491180e6719
MiriamOrtega <120659306+MiriamOrtega@users.noreply.github.com>
2022-12-21 14:12:50 -0600

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -12,6 +12,7 @@ public class DemoApplication {
 
     @RequestMapping("/")
     String home() {
+        System.out.println("Este es el log en nuestro servicio....");
         return "ya casi terminamos uju Miriam";
     }
 
